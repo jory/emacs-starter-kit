20baef4bc1910d90db882194404a1215bffb745f
Jory Graham <jory.graham@gmail.com>
2011-03-03 23:02:19 -0500

    Added two Elisp functions.

--- Jory.el
+++ Jory.el
@@ -10,3 +10,28 @@
       erc-nick "jory"
       erc-user-full-name "Jory Graham"
       erc-input-line-position -1)
+
+(defun eshell/clear ()
+  "Clears the shell buffer ala Unix's clear or DOS' cls"
+  (interactive)
+  ;; the shell prompts are read-only, so clear that for the duration
+  (let ((inhibit-read-only t))
+	;; simply delete the region
+	(delete-region (point-min) (point-max))))
+
+(defun count-words-region (beginning end)
+  "Print number of words in the region."
+  (interactive "r")
+  (message "Counting words in region ... ")
+  (save-excursion
+    (let ((count 0))
+      (goto-char beginning)
+      (while (and (< (point) end)
+                  (re-search-forward "\\w+\\W*" end t))
+        (setq count (1+ count)))
+      (cond ((zerop count)
+             (message "The region does NOT have any words."))
+            ((= 1 count)
+             (message "The region has 1 word."))
+            (t (message "The region has %d words." count))))))
+
