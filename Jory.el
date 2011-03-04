c3dbbab2dfe0ebd3f7d53f5e10e95ac8f3ac9731
Jory Graham <jory.graham@gmail.com>
2011-03-03 23:01:26 -0500

    Added ERC customizations.

--- Jory.el
+++ Jory.el
@@ -2,3 +2,11 @@
 
 (if (eq system-type 'windows-nt)
     (setq magit-git-executable "C:/Program Files (x86)/Git/bin/git.exe"))
+(setq erc-hide-list (quote ("JOIN" "PART" "QUIT"))
+      erc-modules (quote (autojoin button completion fill irccontrols
+                                   list match menu move-to-prompt netsplit
+                                   networks noncommands readonly ring
+                                   scrolltobottom stamp track))
+      erc-nick "jory"
+      erc-user-full-name "Jory Graham"
+      erc-input-line-position -1)
