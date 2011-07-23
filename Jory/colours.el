7db41edde9fc72086531454197347c38ebf1388b
Jory Graham <jory.graham@gmail.com>
2011-07-22 21:44:24 -0400

    Trying to fix the requires for color-themes.

--- Jory/colours.el
+++ Jory/colours.el
@@ -1,9 +1,15 @@
+(require 'color-theme)
+(require 'color-theme-github)
+(require 'color-theme-railscasts)
 (require 'color-theme-solarized)
+(require 'color-theme-zenburn)
+
 (defalias 'zen 'color-theme-zenburn)
 (defalias 'dark 'color-theme-solarized-dark)
 (defalias 'light 'color-theme-solarized-light)
-(zenburn)
 
 (global-set-key (kbd "C-c c z") 'zen)
 (global-set-key (kbd "C-c c d") 'dark)
 (global-set-key (kbd "C-c c l") 'light)
+
+(color-theme-zenburn)
